f4f0dea7bd6ccd6821d980dc7ac632c1275ca75a
nikitanatalenko <47998842+nikitanatalenko@users.noreply.github.com>
2019-02-26 09:00:51 +0700

    Add files via upload

---
+++ sp_lr1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+	int a = 4, b = 5, c = a + b;
+	cout << a << endl << b << endl << c << endl;
+	system("pause");
+	return 0;
+}
